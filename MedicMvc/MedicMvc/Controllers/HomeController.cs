1301da3798cc05ac436721a5af3d982836920cef
jone0751 <jone0751@umn.edu>
2014-06-18 12:14:16 -0500

    Migrating site to MVC. Put in new directiory

---
+++ MedicMvc/MedicMvc/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedicMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult Communicate()
+        {
+            ViewBag.Message = "Communicate with Doctors and Nurses";
+
+            return View();
+        }
+
+        public ActionResult Medicine()
+        {
+            ViewBag.Message = "Medicine info page.";
+
+            return View();
+        }
+
+        public ActionResult Discharge()
+        {
+            ViewBag.Message = "Discharge informationn page.";
+
+            return View();
+        }
+
+        public ActionResult Rating()
+        {
+            ViewBag.Message = "Hospital Rating page.";
+
+            return View();
+        }
+
+        public ActionResult Pain()
+        {
+            ViewBag.Message = "Pain Management page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
